5d05902e105a323f264243d967ba1b05315a9d23
Andreas Hofmann <mail@andreas-hofmann.org>
2020-05-04 09:55:54 +0200

    Evaluate plot-enabled setting when drawing graphs.
    Signed-off-by: Andreas Hofmann <mail@andreas-hofmann.org>

--- slogger/static/js/app.js
+++ slogger/static/js/app.js
@@ -1,12 +1,12 @@
-function load_time_graph(element, url, get) {
+function load_time_graph(element, url, get, options) {
     $.get(url, get,
         function(data) {
             Plotly.newPlot(element, [
-                    { name: 'Night (h)', y: data['night_h'], text: data['night_cnt'], marker: { color:'rgb(100,000,230)' }, x: data['day'], type: 'bar'  },
-                    { name: 'Day (h)',   y: data['day_h'],   text: data['day_cnt'],   marker: { color:'rgb(245,200,050)' }, x: data['day'], type: 'bar'  },
-                    { name: 'Total (h)', y: data['sum_h'],   text: data['sum_cnt'],   mode: 'markers', marker: { color:'rgb(000,000,000)' }, x: data['day'], type: 'scatter' },
-                    { name: 'Diapers',   y: data['diapers'],                          mode: 'markers', marker: { color:'rgb(0,200,050)' }, x: data['day'], type: 'scatter'  },
-                    { name: 'Meals',     y: data['meals'],                            mode: 'markers', marker: { color:'rgb(245,0,050)' }, x: data['day'], type: 'scatter'  },
+                    { name: 'Night (h)', y: data['night_h'], text: data['night_cnt'], marker: { color:'rgb(100,000,230)' }, x: data['day'], type: 'bar', visible: options.plots.sleep,  },
+                    { name: 'Day (h)',   y: data['day_h'],   text: data['day_cnt'],   marker: { color:'rgb(245,200,050)' }, x: data['day'], type: 'bar', visible: options.plots.sleep,  },
+                    { name: 'Total (h)', y: data['sum_h'],   text: data['sum_cnt'],   mode: 'markers', marker: { color:'rgb(000,000,000)' }, x: data['day'], type: 'scatter', visible: options.plots.sleep, },
+                    { name: 'Diapers',   y: data['diapers'],                          mode: 'markers', marker: { color:'rgb(0,200,050)' },   x: data['day'], type: 'scatter', visible: options.plots.diapers  },
+                    { name: 'Meals',     y: data['meals'],                            mode: 'markers', marker: { color:'rgb(245,0,050)' },   x: data['day'], type: 'scatter', visible: options.plots.meals, },
                 ], {
                     yaxis: {
                         showticklabels:true,
@@ -21,13 +21,13 @@ function load_time_graph(element, url, get) {
     );
 }
 
-function load_histogram(element, url, get) {
+function load_histogram(element, url, get, options) {
     $.get(url, get,
         function(data) {
             Plotly.newPlot(element, [
-                    { name: 'Sleep',   type: 'histogram', histfunc: "sum", x: data['time'], y: data['sleep'] },
-                    { name: 'Meals',   type: 'histogram', histfunc: "sum", x: data['time'], y: data['meals'] },
-                    { name: 'Diapers', type: 'histogram', histfunc: "sum", x: data['time'], y: data['diapers'] },
+                    { name: 'Sleep',   type: 'histogram', histfunc: "sum", x: data['time'], y: data['sleep'],   visible: options.plots.sleep },
+                    { name: 'Meals',   type: 'histogram', histfunc: "sum", x: data['time'], y: data['meals'],   visible: options.plots.meals },
+                    { name: 'Diapers', type: 'histogram', histfunc: "sum", x: data['time'], y: data['diapers'], visible: options.plots.diapers },
                 ], {
                     xaxis: {
                         autotick:false,
