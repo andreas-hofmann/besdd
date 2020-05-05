026464905c81f3587c68b9273a726e7642b631a7
Andreas Hofmann <mail@andreas-hofmann.org>
2020-05-05 15:21:49 +0200

    admin.py: Added UserSettings inline to UserAdmin.
    Signed-off-by: Andreas Hofmann <mail@andreas-hofmann.org>

--- slogger/admin.py
+++ slogger/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
 from .models import *
+
+class UserSettingsInline(admin.StackedInline):
+    model = UserSettings
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (UserSettingsInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 admin.site.register(UserSettings)
 admin.site.register(Child)
