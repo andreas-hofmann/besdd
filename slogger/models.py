94e0bb7f00c5ec170fa8e398fea618f1f470e93c
Andreas Hofmann <mail@andreas-hofmann.org>
2020-05-25 08:40:59 +0200

    models.py: Use new UI by default in UserSettings.
    Signed-off-by: Andreas Hofmann <mail@andreas-hofmann.org>

--- slogger/models.py
+++ slogger/models.py
@@ -28,7 +28,7 @@ class UserSettings(models.Model,
     histogram_raster = models.IntegerField("Time raster for histogram (minutes)", default=10)
     histogram_factor_md = models.IntegerField("Time factor for meals+diapers in histogram", default=6)
 
-    use_new_ui = models.BooleanField("Use new UI variant", default=False);
+    use_new_ui = models.BooleanField("Use new UI variant", default=True);
 
     def __str__(self):
         return f"Settings for { self.user }"
